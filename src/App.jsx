4e352ca4b4c1355c2e4470703a8b734573c1886a
Manas Charchi <charchimanas2018@gmail.com>
2025-12-30 08:19:51 +0530

    modifing according cookies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthSuccess from "./AuthSuccess";
-import Dashboard from "./Dashboard";
 import Login from "./pages/Login.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<h1>hi</h1>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/auth/success" element={<AuthSuccess />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,15 +27,9 @@ export default function Login() {
       const data = await res.json();
 
       if (res.ok) {
-        // Save tokens in localStorage
-        localStorage.setItem("access_token", data.access);
-        localStorage.setItem("refresh_token", data.refresh);
-        localStorage.setItem("user", JSON.stringify(data.user));
-
-        // Redirect to dashboard/home
         navigate("/");
       } else {
-        setError(data.error || "Login failed");
+        setError(data.message);
       }
     } catch (err) {
       console.error(err);
